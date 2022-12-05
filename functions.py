ee1dec9a42e7e4bf473613f7390cb6682567a531
Ayoub NAINIA <nainiaayoub6@gmail.com>
2022-12-05 10:49:05 +0100

    roll back

--- functions.py
+++ functions.py
@@ -2,7 +2,6 @@ import streamlit as st
 import re
 import pandas as pd
 import emoji
-from arabert.preprocess import ArabertPreprocessor
 
 @st.cache
 def clean_text(text):
@@ -23,23 +22,8 @@ def clean_text(text):
     return text
 
 @st.cache
-def load_arabert():
-    model_name = "aubmindlab/bert-base-arabertv2"
-    arabert_prep = ArabertPreprocessor(model_name=model_name)
-    return arabert_prep
-
-@st.cache
-def preprocess_text(text):
-  text = re.sub("[a-zA-Z]", " ", text) # remove english letters
-  text = re.sub(r'[ء-ي]+@', '', text) # remove profile tags
-  text = re.sub(r'[ء-ي]+#', '', text) # remove profile tags
-  arabert_prep = load_arabert()
-  text = arabert_prep.preprocess(text)
-  return text
-
-
-def get_prediction(text, model):
-    result = model(text).cats
+def get_prediction(text):
+    result = st.session_state['nlp'](text).cats
     label = sorted(result, key=result.get, reverse=True)[0]
     score = result[label]
 
@@ -51,23 +35,13 @@ def convert_df(df):
 
 @st.cache
 def classify_from_df(df, column):
-    predicted_classes_off = []
-    scores_off = []
-    predicted_classes_misg = []
-    scores_misg = []
+    predicted_classes = []
+    scores = []
     for t in list(df[column]):
-        text_cleaned = preprocess_text(t)
-        # offensive
-        label, score = get_prediction(text_cleaned, st.session_state['nlp_off'])
-        label = label.replace('_', ' ')
-        predicted_classes_off.append(label)
-        scores_off.append(score)
-        # misogyny
-        label, score = get_prediction(text_cleaned, st.session_state['nlp_misg'])
-        label_misg = 'not Misogyny' if label == 'none' else 'Misogyny'
-        predicted_classes_misg.append(label_misg)
-        scores_misg.append(score)
-    
-    
-    df_output = pd.DataFrame(list(zip(list(df[column]), predicted_classes_off, scores_off, predicted_classes_misg, scores_misg)), columns=['Text', 'Offensiveness (predicted)', 'Score (Offensiveness)', 'Misogyny  (predicted)', 'Score (Misogyny)'])
+        text_cleaned = clean_text(t)
+        label, score = get_prediction(text_cleaned)
+        predicted_classes.append(label)
+        scores.append(score)
+
+    df_output = pd.DataFrame(list(zip(list(df[column]), predicted_classes, scores)), columns=['Text', 'Class', 'Score'])
     return df_output
