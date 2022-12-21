e7533587269109f62041579064f0e66ed29093d4
Ayoub NAINIA <nainiaayoub6@gmail.com>
2022-12-21 22:22:49 +0100

    update classify_from_df

--- functions.py
+++ functions.py
@@ -2,7 +2,13 @@ import streamlit as st
 import re
 import pandas as pd
 import emoji
+import os
+import spacy
 from arabert.preprocess import ArabertPreprocessor
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score, classification_report
 
 @st.cache
 def clean_text(text):
@@ -50,11 +56,42 @@ def convert_df(df):
     return df.to_csv().encode('utf-8')
 
 @st.cache
+def compute_score(target, predicted, score):
+    if score == 'accuracy':
+        return accuracy_score(target, predicted)
+    elif score == 'recall':
+        return recall_score(target, predicted, pos_label='not Misogyny')
+    elif score == 'precision':
+        return precision_score(target, predicted, pos_label='not Misogyny')
+    elif score == 'f1_score':
+        return f1_score(target, predicted, pos_label='not Misogyny')
+
+@st.cache
 def classify_from_df(df, column):
     predicted_classes_off = []
     scores_off = []
-    predicted_classes_misg = []
+    letmi = []
+    armis = []
+    letmi_armis = []
+    fb_votes = []
     scores_misg = []
+    path = './models/vaw_misogyny_models/'
+    misg = {}
+    misg_paths = []
+    models = []
+    # get misogyny models
+    for i in os.listdir(path):
+        models.append(i)
+        misg_paths.append(path+i)
+        
+    # load misogyny models
+    loaded_misg_models = []
+    for i in misg_paths:
+        loaded_misg_models.append(spacy.load(i))
+        
+    # classify with models
+    predictions_misg = []
+    
     for t in list(df[column]):
         text_cleaned = preprocess_text(t)
         # offensive
@@ -63,11 +100,16 @@ def classify_from_df(df, column):
         predicted_classes_off.append(label)
         scores_off.append(score)
         # misogyny
-        label, score = get_prediction(text_cleaned, st.session_state['nlp_misg'])
-        label_misg = 'not Misogyny' if label == 'none' else 'Misogyny'
-        predicted_classes_misg.append(label_misg)
-        scores_misg.append(score)
+        for i in range(len(loaded_misg_models)):
+            label_misg, score_misg = get_prediction(text_cleaned, loaded_misg_models[i])
+            label_misg = 'not Misogyny' if label_misg == 'none' else 'Misogyny'
+            if models[i] not in misg.keys():
+                misg[models[i]] = []
+
+            misg[models[i]].append(label_misg)
+
+        
     
     
-    df_output = pd.DataFrame(list(zip(list(df[column]), predicted_classes_off, scores_off, predicted_classes_misg, scores_misg)), columns=['Text', 'Offensiveness (predicted)', 'Score (Offensiveness)', 'Misogyny  (predicted)', 'Score (Misogyny)'])
+    df_output = pd.DataFrame(list(zip(list(df[column]), predicted_classes_off, scores_off, misg['ArMIS'], misg['LetMI'], misg['LetMI_ArMIS'], misg['Model_fb_data_votes'])), columns=['Text', 'Offensiveness (predicted)', 'Score (Offensiveness)', 'Misogyny (ArMIS)', 'Misogyny (LetMI)', 'Misogyny (LetMI_ArMIS)', 'Misogyny (Model_fb_data_votes)'])
     return df_output
