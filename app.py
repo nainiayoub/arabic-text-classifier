078803f753b04b4a7460ebcce709c2af3d09183e
Ayoub NAINIA <nainiaayoub6@gmail.com>
2022-12-21 22:22:01 +0100

    add all misg models

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import spacy
-from functions import clean_text, get_prediction, convert_df, classify_from_df, load_arabert, preprocess_text
+from functions import clean_text, get_prediction, convert_df, classify_from_df, load_arabert, preprocess_text, compute_score
 import pandas as pd
+import os
 from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score
 
 
@@ -54,7 +55,7 @@ if input_options == 'Text':
         label_off, score_off = get_prediction(text_cleaned_off, st.session_state['nlp_off'])
         text_cleaned_misg = preprocess_text(text)
         label_misg, score_misg = get_prediction(text_cleaned_misg, st.session_state['nlp_misg'])
-        label_misg = 'not_misogynistic' if label_misg == 'none' else 'misogynistic'
+        label_misg = 'not Misogyny' if label_misg == 'none' else 'Misogyny'
         col1, col2 = st.columns(2)
         with col1:
             st.info(label_off)
@@ -62,7 +63,33 @@ if input_options == 'Text':
         with col2:
             st.warning("Score: "+str(score_off))
             st.warning("Score: "+str(score_misg))
-        
+
+        with st.expander("See more - Misogyny"):
+            st.info('Selected Model: LetMI')
+            path = './models/vaw_misogyny_models/'
+            misg_paths = []
+            models = []
+            # get misogyny models
+            for i in os.listdir(path):
+                models.append(i)
+                misg_paths.append(path+i)
+                
+            # load misogyny models
+            loaded_misg_models = []
+            for i in misg_paths:
+                loaded_misg_models.append(spacy.load(i))
+                
+            # classify with models
+            predictions_misg = []
+            scores_misg = []
+            for i in loaded_misg_models:
+                label_misg, score_misg = get_prediction(text_cleaned_misg, i)
+                label_misg = 'not Misogyny' if label_misg == 'none' else 'Misogyny'
+                scores_misg.append(score_misg)
+                predictions_misg.append(label_misg)
+            txt_column = [text]*4
+            df_models_misg = pd.DataFrame(list(zip(txt_column, models, predictions_misg, scores_misg)), columns=['Text', 'Model', 'Misogyny/not Misogyny', 'Score'])
+            st.dataframe(df_models_misg)
 
         
         
@@ -100,12 +127,12 @@ else:
                 """)
                 target_off = st.selectbox('Select target class for offensiveness', ['None']+columns_tar)
                 predicted_off = st.selectbox('Select predicted class for offensiveness', ['None']+columns_pre)
-                st.markdown(html_temp.format("rgba(55, 53, 47, 0.16)"),unsafe_allow_html=True)
-                st.markdown("""
-                ## Evaluating Misogyny
-                """)
-                target_misg = st.selectbox('Select target class for misogyny', ['None']+columns_tar)
-                predicted_misg = st.selectbox('Select predicted class for misogyny', ['None']+columns_pre)
+                # st.markdown(html_temp.format("rgba(55, 53, 47, 0.16)"),unsafe_allow_html=True)
+                # st.markdown("""
+                # ## Evaluating Misogyny
+                # """)
+                # target_misg = st.selectbox('Select target class for misogyny', ['None']+columns_tar)
+                # predicted_misg = st.selectbox('Select predicted class for misogyny', ['None']+columns_pre)
 
             if target_off != 'None' and predicted_off != 'None':
                 target = list(dataframe[target_off])
@@ -123,26 +150,25 @@ else:
                 with col4:
                     st.metric(label="F1-score", value=str('{:.4f}'.format(f1_score(target, predicted, pos_label='offensive'))))
 
-            if target_misg != 'None' and predicted_misg != 'None':
-                target = list(dataframe[target_misg])
-                predicted = list(df_output[predicted_misg])
-                st.markdown("""
-                    #### Evaluating Misogyny
-                """)
-                col1, col2, col3, col4 = st.columns(4)
-                with col1:
-                    st.metric(label="Accuracy", value=str('{:.4f}'.format(accuracy_score(target, predicted))))
-                with col2:
-                    st.metric(label="Precision", value=str('{:.4f}'.format(precision_score(target, predicted, pos_label='Misogyny'))))
-                with col3:    
-                    st.metric(label="Recall", value=str('{:.4f}'.format(recall_score(target, predicted, pos_label='Misogyny'))))
-                with col4:
-                    st.metric(label="F1-score", value=str('{:.4f}'.format(f1_score(target, predicted, pos_label='Misogyny'))))
-                    
-               
-
-
-
-
-
-
+            # if target_misg != 'None':
+            #     target = list(dataframe[target_misg])
+            #     armis = list(df_output['Misogyny (ArMIS)'])
+            #     letmi = list(df_output['Misogyny (LetMI)'])
+            #     letmi_armis = list(df_output['Misogyny (LetMI_ArMIS)'])
+            #     fb = list(df_output['Misogyny (Model_fb_data_votes)'])
+            #     st.markdown("""
+            #         #### Evaluating Misogyny
+            #     """)
+            #     model_names = ['LetMI', 'ArMIS', 'LetMI_ArMIS', 'FB_Trained']
+            #     scores = ['accuracy', 'recall', 'precision', 'f1_score']
+            #     dict_scores = {}
+            #     # ours
+            #     for s in scores:
+            #         if s not in dict_scores.keys():
+            #             dict_scores[s] = []
+            #         dict_scores[s].append(compute_score(target, letmi, s))
+            #         dict_scores[s].append(compute_score(target, armis, s))
+            #         dict_scores[s].append(compute_score(target, letmi_armis, s))
+            #         dict_scores[s].append(compute_score(target, fb, s))
+            #     df_scores = pd.DataFrame(list(zip(model_names, dict_scores['accuracy'], dict_scores['recall'], dict_scores['precision'], dict_scores['f1_score'])), columns = ['Models', 'accuracy', 'recall', 'precision', 'f1_score'])
+            #     st.table(df_scores)
